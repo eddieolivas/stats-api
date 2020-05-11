50bece2bc741689175da44426c499b70b94e120d
Eddie Olivas <eddie@Eddies-MBP.localdomain>
2020-05-11 09:09:31 -0700

    Added auth to read location data.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 require('./db/mongoose');
 const Location = require('./models/location');
+const User = require('./models/user');
 const locationRouter = require('./routers/location');
+const userRouter = require('./routers/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(locationRouter);
+app.use(userRouter);
 
 app.listen(port, () => {
     console.log('COVID-19 stats API server is up on port ' + port);
