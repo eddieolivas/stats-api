63bac10c27a0c9240c2721133e9cf77a77d76a8b
Eddie Olivas <eddie@Eddies-MBP.localdomain>
2020-05-12 11:28:44 -0700

    Updated table with new stat columns.

--- src/routers/location.js
+++ src/routers/location.js
@@ -14,6 +14,7 @@ router.get('/locations/:locationId', auth, async (req, res) => {
     }
 
     $name = location.name;
+    $dateUpdated = location.dateUpdated;
     $state = location.state;
     $beds = location.beds;
     $positiveresidents = location.positiveresidents;
@@ -21,10 +22,8 @@ router.get('/locations/:locationId', auth, async (req, res) => {
     $positivestaff = location.positivestaff;
     $totalpositivestaff = location.totalpositivestaff;
     $symptomsin72hours = location.symptomsin72hours;
-    $dateUpdated = location.dateUpdated;
 
-
-    const response = '<div style="display: none;"><div id="covid-stats"><h3><strong>Current COVID-19 Testing Information for ' + $name + '</strong></h3><p>Data reflects current information as of ' + $dateUpdated + '.</p><ul><li>A positive test confirms evidence of the virus, but does not confirm the patient is experiencing symptoms.</li><li>A negative result indicates that a test has been administered and the result was negative for COVID-19.</li><li>Pending tests include retests of current or former positives as two negative results in consecutive days is the current standard for determining when someone is no longer positive.</li></ul></p><table><thead><tr><th>Operational Beds</th><th>Tests Pending</th><th>Tests Negative</th><th>Active Positives</th></tr></thead><tbody><tr><td>' + $beds + '</td><td>' + $positiveresidents + '</td><td>' + $totalpositiveresidents + '</td><td>' + $totalpositivestaff + '</td></tr></tbody></table></div></div>';
+    const response = '<div style="display: none;"><div id="covid-stats"><h3><strong>Current COVID-19 Testing Information for ' + $name + '</strong></h3><p>Data reflects current information as of ' + $dateUpdated + '.</p><ul><li>A positive test confirms evidence of the virus, but does not confirm the patient is experiencing symptoms.</li><li>A negative result indicates that a test has been administered and the result was negative for COVID-19.</li><li>Pending tests include retests of current or former positives as two negative results in consecutive days is the current standard for determining when someone is no longer positive.</li></ul></p><table><thead><tr><th>Operational Beds</th><th>New Positive Covid Resident Cases Since Prior Day</th><th>Cumulative Total of Covid Positive Residents</th><th>New Positive Covid Staff Cases Since Prior Day</th><th>Cumulative Total Number of Covid Positive Staff</th><th>Incidents of 3 or More Residents/Staff with New Respiratory Symptoms Within 72 Hours</th></tr></thead><tbody><tr><td>' + $beds + '</td><td>' + $positiveresidents + '</td><td>' + $totalpositiveresidents + '</td><td>' + $positivestaff + '</td><td>' + $totalpositivestaff + '</td><td>' + $symptomsin72hours + '</td></tr></tbody></table></div></div>';
 
     res.status(201).send(response);
   } catch (e) {
