553f4c1051ff44919fce4d7f5095d616e7dc3e17
Eddie Olivas <eddie@Eddies-MBP.localdomain>
2020-05-12 12:08:53 -0700

    Updated database password.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://dbUser:Marvino03110311!!@stats-api-6abbd.mongodb.net/stats-api?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://dbUser:HaeQHxqPhSfJZqQi@stats-api-6abbd.mongodb.net/stats-api?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
